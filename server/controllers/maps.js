534dd5dac0189a2ec41266b3e44dfdcac3646728
Nfey <nfey1@me.com>
2020-12-20 23:32:46 -0800

    Added join function

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -1,4 +1,5 @@
 const Map = require('../models/map').model;
+const User = require('../models/user');
 const Pixel = require('../models/pixel').model;
 module.exports = {
     getAll: (req, res) => {
@@ -33,6 +34,21 @@ module.exports = {
                 res.json(map);
             })
             .catch(e => res.status(422).json(e));
+    },
+    join: (req, res) => {
+        Map.findById(req.params.id)
+            .then(map => {
+                User.findById(req.user._id)
+                    .then(user => {
+                        user.maps.push(map._id);
+                        user.save();
+                        map.users.push(user._id);
+                        map.save();
+                        res.json()
+                    })
+                    .catch(e => res.status(420).json(e))
+            })
+            .catch(e => res.status(422).json(e))
     },
     update: (req, res) => {
         Map.findByIdAndUpdate(req.params.id, req.body, { useFindAndModify: false })
