fc4b9f536de4661c5440c017e001fc6442c0955d
Nfey <nfey1@me.com>
2021-01-05 16:15:11 -0800

    added phase property to maps

--- server/models/map.js
+++ server/models/map.js
@@ -4,7 +4,8 @@ var MapSchema = new mongoose.Schema({
     height: { type: Number, required: true, default: 10 },
     width: { type: Number, required: true, default: 10 },
     pixels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Pixel" }],
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    phase: { type: String, default: "turn" }
 }, { timestamps: true });
 const Map = mongoose.model("Map", MapSchema);
 module.exports = {
